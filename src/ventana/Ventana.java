6ea0251da30d8412d75c8923661823d0ca2be270
jorgeamercg <jorgeamercg@gmail.com>
2025-02-20 05:10:11 -0700

    unidad 2 ejercicio 16 o diseño de calculadora

--- src/ventana/Ventana.java
+++ src/ventana/Ventana.java
@@ -5,6 +5,7 @@ package ventana;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -26,7 +27,8 @@ public class Ventana extends JFrame{
 	
 	//Fuentes personalizadas
 	Font etiquetas = new Font("Romana", Font.ROMAN_BASELINE, 22);
-	Font etiquetas2 = new Font("Romana", Font.ROMAN_BASELINE, 10);
+	Font etiquetas2 = new Font("Romana 2", Font.ROMAN_BASELINE, 10);
+	/*Font etiquetas3 = new Font("Romana 3", Font.ROMAN_BASELINE, 10);*/
 
 	//CONSTRUCTOR
 	public Ventana(String title) {
@@ -49,9 +51,14 @@ public class Ventana extends JFrame{
 		this.setMinimumSize(new Dimension(400, 400));
 		this.setMaximumSize(new Dimension(600, 600));
 		
-		/*this.add(this.login());*/
+		/*//Iniciar Sesión
+		this.add(this.login());*/
 		
-		this.add(this.registro());
+		/*//Registrarse
+		this.add(this.registro());*/
+		
+		//Calculadora
+		this.add(this.calculadora());
 		
 		this.repaint();
 		this.revalidate();
@@ -123,7 +130,7 @@ public class Ventana extends JFrame{
 		JButton access = new JButton("Acceder");
 		access.setBounds(165, 350, 150, 40);
 		access.setFont(etiquetas);
-		etiqueta2.setHorizontalAlignment(JLabel.CENTER);
+		access.setHorizontalAlignment(JLabel.CENTER);
 		access.setBackground(Color.ORANGE);
 		access.setOpaque(true);
 		login.add(access);
@@ -161,7 +168,6 @@ public class Ventana extends JFrame{
 		registro.setLocation(0, 0);
 		registro.setSize(500, 500);
 		registro.setOpaque(true);
-		/*login.setBackground(Color.RED);*/
 		registro.setBackground(Color.WHITE);
 		registro.setVisible(true);
 		registro.setLayout(null);
@@ -179,8 +185,6 @@ public class Ventana extends JFrame{
 		
 		JLabel etiqueta2 = new JLabel("Email");
 		etiqueta2.setBounds(190, 65, 100, 30);
-		/*etiqueta2.setSize(80, 30);*/
-		/*etiqueta2.setLocation(60, 150);*/
 		etiqueta2.setFont(etiquetas);
 		etiqueta2.setHorizontalAlignment(JLabel.CENTER);
 		etiqueta2.setBackground(Color.YELLOW);
@@ -229,11 +233,11 @@ public class Ventana extends JFrame{
 		biografia.setOpaque(true);
 		registro.add(biografia);
 		
-		//Botones
+		//Botón
 		JButton access = new JButton("Crear cuenta");
 		access.setBounds(140, 400, 200, 30);
 		access.setFont(etiquetas);
-		etiqueta2.setHorizontalAlignment(JLabel.CENTER);
+		access.setHorizontalAlignment(JLabel.CENTER);
 		access.setBackground(Color.ORANGE);
 		access.setOpaque(true);
 		registro.add(access);
@@ -306,7 +310,220 @@ public class Ventana extends JFrame{
 		terminos.add(terms1);
 		terminos.add(terms2);
 
+		registro.revalidate();
+		
 		return registro;
+		
+	}
+	
+	public JPanel calculadora() {
+		
+		//CREACIÓN DE OBJETOS
+		
+		//Panel
+		JPanel calculadora = new JPanel();
+		calculadora.setLocation(0, 0);
+		calculadora.setSize(500, 500);
+		calculadora.setOpaque(true);
+		calculadora.setBackground(Color.BLACK);
+		calculadora.setVisible(true);
+		calculadora.setLayout(null);
+		
+		//Etiqueta
+		JLabel pantalla = new JLabel("777.0");
+		pantalla.setBounds(21, 20, 443, 50);
+		pantalla.setFont(etiquetas);
+		pantalla.setHorizontalAlignment(JLabel.CENTER);
+		pantalla.setBackground(Color.WHITE);
+		pantalla.setOpaque(true);
+		calculadora.add(pantalla);
+		
+		//Botones
+		JButton b1 = new JButton("CE");
+		b1.setBounds(21, 75, 107, 70);
+		b1.setFont(etiquetas);
+		b1.setHorizontalAlignment(JLabel.CENTER);
+		b1.setBackground(Color.YELLOW);
+		b1.setOpaque(true);
+		b1.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b1);
+		
+		JButton b2 = new JButton();
+		b2.setBounds(133, 75, 107, 70);
+		b2.setFont(etiquetas);
+		b2.setHorizontalAlignment(JLabel.CENTER);
+		b2.setBackground(Color.lightGray);
+		b2.setOpaque(true);
+		b2.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b2);
+		
+		JButton b3 = new JButton();
+		b3.setBounds(245, 75, 107, 70);
+		b3.setFont(etiquetas);
+		b3.setHorizontalAlignment(JLabel.CENTER);
+		b3.setBackground(Color.lightGray);
+		b3.setOpaque(true);
+		b3.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b3);
+		
+		JButton b4 = new JButton();
+		b4.setBounds(357, 75, 107, 70);
+		b4.setFont(etiquetas);
+		b4.setHorizontalAlignment(JLabel.CENTER);
+		b4.setBackground(Color.lightGray);
+		b4.setOpaque(true);
+		b4.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b4);
+		
+		JButton b5 = new JButton("7");
+		b5.setBounds(21, 150, 107, 70);
+		b5.setFont(etiquetas);
+		b5.setHorizontalAlignment(JLabel.CENTER);
+		b5.setBackground(Color.lightGray);
+		b5.setOpaque(true);
+		b5.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b5);
+		
+		JButton b6 = new JButton("8");
+		b6.setBounds(133, 150, 107, 70);
+		b6.setFont(etiquetas);
+		b6.setHorizontalAlignment(JLabel.CENTER);
+		b6.setBackground(Color.lightGray);
+		b6.setOpaque(true);
+		b6.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b6);
+		
+		JButton b7 = new JButton("9");
+		b7.setBounds(245, 150, 107, 70);
+		b7.setFont(etiquetas);
+		b7.setHorizontalAlignment(JLabel.CENTER);
+		b7.setBackground(Color.lightGray);
+		b7.setOpaque(true);
+		b7.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b7);
+		
+		JButton b8 = new JButton("/");
+		b8.setBounds(357, 150, 107, 70);
+		b8.setFont(etiquetas);
+		b8.setHorizontalAlignment(JLabel.CENTER);
+		b8.setBackground(Color.ORANGE);
+		b8.setOpaque(true);
+		b8.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b8);
+		
+		JButton b9 = new JButton("4");
+		b9.setBounds(21, 225, 107, 70);
+		b9.setFont(etiquetas);
+		b9.setHorizontalAlignment(JLabel.CENTER);
+		b9.setBackground(Color.lightGray);
+		b9.setOpaque(true);
+		b9.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b9);
+		
+		JButton b10 = new JButton("5");
+		b10.setBounds(133, 225, 107, 70);
+		b10.setFont(etiquetas);
+		b10.setHorizontalAlignment(JLabel.CENTER);
+		b10.setBackground(Color.lightGray);
+		b10.setOpaque(true);
+		b10.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b10);
+		
+		JButton b11 = new JButton("6");
+		b11.setBounds(245, 225, 107, 70);
+		b11.setFont(etiquetas);
+		b11.setHorizontalAlignment(JLabel.CENTER);
+		b11.setBackground(Color.lightGray);
+		b11.setOpaque(true);
+		b11.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b11);
+		
+		JButton b12 = new JButton("*");
+		b12.setBounds(357, 225, 107, 70);
+		b12.setFont(etiquetas);
+		b12.setHorizontalAlignment(JLabel.CENTER);
+		b12.setBackground(Color.ORANGE);
+		b12.setOpaque(true);
+		b12.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b12);
+		
+		JButton b13 = new JButton("1");
+		b13.setBounds(21, 300, 107, 70);
+		b13.setFont(etiquetas);
+		b13.setHorizontalAlignment(JLabel.CENTER);
+		b13.setBackground(Color.lightGray);
+		b13.setOpaque(true);
+		b13.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b13);
+		
+		JButton b14 = new JButton("2");
+		b14.setBounds(133, 300, 107, 70);
+		b14.setFont(etiquetas);
+		b14.setHorizontalAlignment(JLabel.CENTER);
+		b14.setBackground(Color.lightGray);
+		b14.setOpaque(true);
+		b14.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b14);
+		
+		JButton b15 = new JButton("3");
+		b15.setBounds(245, 300, 107, 70);
+		b15.setFont(etiquetas);
+		b15.setHorizontalAlignment(JLabel.CENTER);
+		b15.setBackground(Color.lightGray);
+		b15.setOpaque(true);
+		b15.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b15);
+		
+		JButton b16 = new JButton("-");
+		b16.setBounds(357, 300, 107, 70);
+		b16.setFont(etiquetas);
+		b16.setHorizontalAlignment(JLabel.CENTER);
+		b16.setBackground(Color.ORANGE);
+		b16.setOpaque(true);
+		b16.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b16);
+		
+		JButton b17 = new JButton("0");
+		b17.setBounds(21, 375, 107, 70);
+		b17.setFont(etiquetas);
+		b17.setHorizontalAlignment(JLabel.CENTER);
+		b17.setBackground(Color.lightGray);
+		b17.setOpaque(true);
+		b17.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b17);
+		
+		JButton b18 = new JButton(".");
+		b18.setBounds(133, 375, 107, 70);
+		b18.setFont(etiquetas);
+		b18.setHorizontalAlignment(JLabel.CENTER);
+		b18.setBackground(Color.lightGray);
+		b18.setOpaque(true);
+		b18.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b18);
+		
+		JButton b19 = new JButton("=");
+		b19.setBounds(245, 375, 107, 70);
+		b19.setFont(etiquetas);
+		b19.setHorizontalAlignment(JLabel.CENTER);
+		b19.setBackground(Color.ORANGE);
+		b19.setOpaque(true);
+		b19.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b19);
+		
+		JButton b20 = new JButton("+");
+		b20.setBounds(357, 375, 107, 70);
+		b20.setFont(etiquetas);
+		b20.setHorizontalAlignment(JLabel.CENTER);
+		b20.setBackground(Color.ORANGE);
+		b20.setOpaque(true);
+		b20.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		calculadora.add(b20);
+		
+		/*calculadora.setLayout(new GridLayout(5, 4, 10, 10));*/
+		
+		calculadora.revalidate();
+		
+		return calculadora;
 		
 	}
 
