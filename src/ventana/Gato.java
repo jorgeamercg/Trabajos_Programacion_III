7f563ae0d8503212218d9fd14f4be640c2b39d18
jorgeamercg <jorgeamercg@gmail.com>
2025-03-19 10:36:29 -0700

    unidad 3 ejercicio 13 o juego gato_V2

--- src/ventana/Gato.java
+++ src/ventana/Gato.java
@@ -18,13 +18,27 @@ public class Gato {
 
 	private JFrame frame;
 	
-	Boolean turno = true;
+	Boolean Turno = true;
 	
-	JButton  btnNewButton;
+	int Contador_Jugadas = 0;
 	
-	JButton  btnNewButton_2;
+	JButton btnNewButton;
+	
+	JButton btnNewButton_1;
+	
+	JButton btnNewButton_2;
 	
 	JButton btnNewButton_3;
+	
+	JButton btnNewButton_4;
+	
+	JButton btnNewButton_5;
+	
+	JButton btnNewButton_6;
+	
+	JButton btnNewButton_7;
+	
+	JButton btnNewButton_8;
 	
 	/**
 	 * Launch the application.
@@ -64,7 +78,7 @@ public class Gato {
 		frame.getContentPane().add(panel);
 		panel.setLayout(new GridLayout(3, 3, 3, 3));
 		
-		JButton btnNewButton = new JButton("");
+		btnNewButton = new JButton("");
 		btnNewButton.setPreferredSize(new Dimension(100, 100));
 		panel.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
@@ -74,24 +88,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
+				
 				
 				btnNewButton.setText(Jugador);
 				btnNewButton.setEnabled(false);
-				btnNewButton.verificarGanador();
-				
+				verificarGanador();
 			}
 			
 		});
 		
-		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1 = new JButton("");
 		panel.add(btnNewButton_1);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			
@@ -100,24 +120,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_1.setText(Jugador);
 				btnNewButton_1.setEnabled(false);
-				btnNewButton_1.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2 = new JButton("");
 		panel.add(btnNewButton_2);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			
@@ -126,24 +152,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_2.setText(Jugador);
 				btnNewButton_2.setEnabled(false);
-				btnNewButton_2.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3 = new JButton("");
 		panel.add(btnNewButton_3);
 		btnNewButton_3.addActionListener(new ActionListener() {
 			
@@ -152,24 +184,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_3.setText(Jugador);
 				btnNewButton_3.setEnabled(false);
-				btnNewButton_3.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4 = new JButton("");
 		panel.add(btnNewButton_4);
 		btnNewButton_4.addActionListener(new ActionListener() {
 			
@@ -178,24 +216,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_4.setText(Jugador);
 				btnNewButton_4.setEnabled(false);
-				btnNewButton_4.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_5 = new JButton("");
+		btnNewButton_5 = new JButton("");
 		panel.add(btnNewButton_5);
 		btnNewButton_5.addActionListener(new ActionListener() {
 			
@@ -204,24 +248,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_5.setText(Jugador);
 				btnNewButton_5.setEnabled(false);
-				btnNewButton_5.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_6 = new JButton("");
+		btnNewButton_6 = new JButton("");
 		panel.add(btnNewButton_6);
 		btnNewButton_6.addActionListener(new ActionListener() {
 			
@@ -230,24 +280,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_6.setText(Jugador);
 				btnNewButton_6.setEnabled(false);
-				btnNewButton_6.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_7 = new JButton("");
+		btnNewButton_7 = new JButton("");
 		panel.add(btnNewButton_7);
 		btnNewButton_7.addActionListener(new ActionListener() {
 			
@@ -256,24 +312,30 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_7.setText(Jugador);
 				btnNewButton_7.setEnabled(false);
-				btnNewButton_7.verificarGanador();
+				verificarGanador();
 				
 			}
 			
 		});
 		
-		JButton btnNewButton_8 = new JButton("");
+		btnNewButton_8 = new JButton("");
 		panel.add(btnNewButton_8);
 		btnNewButton_8.addActionListener(new ActionListener() {
 			
@@ -282,18 +344,24 @@ public class Gato {
 			
 				String Jugador = "X";
 				
-				if (turno) {
-					turno = false;
+				if (Turno) {
+					Turno = false;
+					
 					Jugador = "X";
+					
+					Contador_Jugadas++;
 				}
 				else {
-					turno = true;
+					Turno = true;
+					
 					Jugador = "O";
+					
+					Contador_Jugadas++;
 				}
 				
 				btnNewButton_8.setText(Jugador);
 				btnNewButton_8.setEnabled(false);
-				btnNewButton_8.verificarGanador();
+				verificarGanador();
 				
 			}
 			
@@ -303,15 +371,67 @@ public class Gato {
 
 	public void verificarGanador() {
 		
-		if (btnNewButton.getText().equals(btnNewButton_2.getText()).equals(btnNewButton_3.getText())){
+		//EMPATE
+		if (Contador_Jugadas == 9) {
+			JOptionPane.showMessageDialog(frame,
+				    "Es un Empate",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+		}
+		//HORIZONTAL 1
+		else if (!btnNewButton.getText().equals("") && btnNewButton.getText().equals(btnNewButton_1.getText()) && btnNewButton_1.getText().equals(btnNewButton_2.getText())) {
+			JOptionPane.showMessageDialog(frame,
+				    "Has Ganado",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+			
+		}
+		//HORIZONTAL 2
+		else if (!btnNewButton_3.getText().equals("") && btnNewButton_3.getText().equals(btnNewButton_4.getText()) && btnNewButton_4.getText().equals(btnNewButton_5.getText())) {
 			JOptionPane.showMessageDialog(frame,
 				    "Has Ganado",
 				    "Verificación de Ganador",
 				    JOptionPane.PLAIN_MESSAGE);
 		}
-		else {
+		//HORIZONTAL 3
+		else if (!btnNewButton_6.getText().equals("") && btnNewButton_6.getText().equals(btnNewButton_7.getText()) && btnNewButton_7.getText().equals(btnNewButton_8.getText())) {
 			JOptionPane.showMessageDialog(frame,
-				    "Es un Empate",
+				    "Has Ganado",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+		}
+		//VERTICAL 1
+		else if (!btnNewButton.getText().equals("") && btnNewButton.getText().equals(btnNewButton_3.getText()) && btnNewButton_3.getText().equals(btnNewButton_6.getText())) {
+			JOptionPane.showMessageDialog(frame,
+				    "Has Ganado",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+		}
+		//VERTICAL 2
+		else if (!btnNewButton_1.getText().equals("") && btnNewButton_1.getText().equals(btnNewButton_4.getText()) && btnNewButton_4.getText().equals(btnNewButton_7.getText())) {
+			JOptionPane.showMessageDialog(frame,
+				    "Has Ganado",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+		}
+		//VERTICAL 3
+		else if (!btnNewButton_2.getText().equals("") && btnNewButton_2.getText().equals(btnNewButton_5.getText()) && btnNewButton_5.getText().equals(btnNewButton_8.getText())) {
+			JOptionPane.showMessageDialog(frame,
+				    "Has Ganado",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+		}
+		//DIAGONAL 1
+		else if (!btnNewButton.getText().equals("") && btnNewButton.getText().equals(btnNewButton_4.getText()) && btnNewButton_4.getText().equals(btnNewButton_8.getText())) {
+			JOptionPane.showMessageDialog(frame,
+				    "Has Ganado",
+				    "Verificación de Ganador",
+				    JOptionPane.PLAIN_MESSAGE);
+		}
+		//DIAGONAL 2
+		else if (!btnNewButton_2.getText().equals("") && btnNewButton_2.getText().equals(btnNewButton_4.getText()) && btnNewButton_4.getText().equals(btnNewButton_6.getText())) {
+			JOptionPane.showMessageDialog(frame,
+				    "Has Ganado",
 				    "Verificación de Ganador",
 				    JOptionPane.PLAIN_MESSAGE);
 		}
