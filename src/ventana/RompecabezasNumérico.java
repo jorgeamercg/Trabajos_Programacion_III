79f1ab5abce19500db8f4865c253c855dff899b2
jorgeamercg <jorgeamercg@gmail.com>
2025-03-25 23:19:27 -0700

    unidad 3 ejercicio 16 - función parte 2 del juego de rompecabezas 15 Puzzle_V2

--- src/ventana/RompecabezasNumérico.java
+++ src/ventana/RompecabezasNumérico.java
@@ -79,7 +79,7 @@ public class RompecabezasNumérico {
 		
 		frame = new JFrame();
 		frame.setTitle("Rompecabezas Numérico");
-		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Gato.class.getResource("/ventana/Saturno.png")));//Ícono de ventana personalizado
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(RompecabezasNumérico.class.getResource("/ventana/Saturno.png")));//Ícono de ventana personalizado
 		frame.setBounds(100, 100, 500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -113,6 +113,38 @@ public class RompecabezasNumérico {
 				
 				Collections.shuffle(listaNúmerosTablero);//Revolver números en el tablero
 				
+				btnNewButton.setText(listaNúmerosTablero.get(0));//Reescribir botón 1
+				
+				btnNewButton_1.setText(listaNúmerosTablero.get(1));//Reescribir botón 2
+				
+				btnNewButton_2.setText(listaNúmerosTablero.get(2));//Reescribir botón 3
+				
+				btnNewButton_3.setText(listaNúmerosTablero.get(3));//Reescribir botón 4
+				
+				btnNewButton_4.setText(listaNúmerosTablero.get(4));//Reescribir botón 5
+				
+				btnNewButton_5.setText(listaNúmerosTablero.get(5));//Reescribir botón 6
+				
+				btnNewButton_6.setText(listaNúmerosTablero.get(6));//Reescribir botón 7
+				
+				btnNewButton_7.setText(listaNúmerosTablero.get(7));//Reescribir botón 8
+				
+				btnNewButton_8.setText(listaNúmerosTablero.get(8));//Reescribir botón 9
+				
+				btnNewButton_9.setText(listaNúmerosTablero.get(9));//Reescribir botón 10
+				
+				btnNewButton_10.setText(listaNúmerosTablero.get(10));//Reescribir botón 11
+				
+				btnNewButton_11.setText(listaNúmerosTablero.get(11));//Reescribir botón 12
+				
+				btnNewButton_12.setText(listaNúmerosTablero.get(12));//Reescribir botón 13
+				
+				btnNewButton_13.setText(listaNúmerosTablero.get(13));//Reescribir botón 14
+				
+				btnNewButton_14.setText(listaNúmerosTablero.get(14));//Reescribir botón 15
+				
+				btnNewButton_15.setText(listaNúmerosTablero.get(15));//Reescribir botón 16
+				
 				}
 			
 			});
@@ -132,25 +164,8 @@ public class RompecabezasNumérico {
 		tablero.setLayout(new GridLayout(4, 4, 10, 10));
 		
 		Collections.shuffle(listaNúmerosTablero);//Revolver números en el tablero
-		btnNewButton.setText(listaNúmerosTablero.get(0));
-		btnNewButton_1.setText(listaNúmerosTablero.get(1));
-		btnNewButton_2.setText(listaNúmerosTablero.get(2));
-		btnNewButton_3.setText(listaNúmerosTablero.get(3));
-		btnNewButton_4.setText(listaNúmerosTablero.get(4));
-		btnNewButton_5.setText(listaNúmerosTablero.get(5));
-		btnNewButton_6.setText(listaNúmerosTablero.get(6));
-		btnNewButton_7.setText(listaNúmerosTablero.get(7));
-		btnNewButton_8.setText(listaNúmerosTablero.get(8));
-		btnNewButton_9.setText(listaNúmerosTablero.get(9));
-		btnNewButton_10.setText(listaNúmerosTablero.get(10));
-		btnNewButton_11.setText(listaNúmerosTablero.get(11));
-		btnNewButton_12.setText(listaNúmerosTablero.get(12));
-		btnNewButton_13.setText(listaNúmerosTablero.get(13));
-		btnNewButton_14.setText(listaNúmerosTablero.get(14));
-		btnNewButton_15.setText(listaNúmerosTablero.get(15));
-
-		btnNewButton = new JButton(númerosTablero[0]);//Botón 1
-		//btnNewButton.setText(listaNúmerosTablero.get(0));
+		
+		btnNewButton = new JButton(listaNúmerosTablero.get(0));//Botón 1
 		btnNewButton.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
@@ -160,10 +175,12 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_1.getText().equals("")) {
 					 btnNewButton_1.setText(btnNewButton.getText());
+					 
 					 btnNewButton.setText("");
 				 }
 				 else if(btnNewButton_4.getText().equals("")) {
 					 btnNewButton_4.setText(btnNewButton.getText());
+					 
 					 btnNewButton.setText("");
 				 }
 				 
@@ -175,25 +192,27 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_1 = new JButton(númerosTablero[1]);//Botón 2
-		//btnNewButton_1.setText(listaNúmerosTablero.get(1));
+		btnNewButton_1 = new JButton(listaNúmerosTablero.get(1));//Botón 2
 		btnNewButton_1.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_1);
-		btnNewButton_2.addActionListener(new ActionListener() {
+		btnNewButton_1.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
 				 if(btnNewButton.getText().equals("")) {
 					 btnNewButton.setText(btnNewButton_1.getText());
+					 
 					 btnNewButton_1.setText("");
 				 }
 				 else if(btnNewButton_2.getText().equals("")) {
 					 btnNewButton_2.setText(btnNewButton_1.getText());
+					 
 					 btnNewButton_1.setText("");
 				 }
 				 else if(btnNewButton_5.getText().equals("")) {
 					 btnNewButton_5.setText(btnNewButton_1.getText());
+					 
 					 btnNewButton_1.setText("");
 				 }
 				 
@@ -205,8 +224,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_2 = new JButton(númerosTablero[2]);//Botón 3
-		//btnNewButton_2.setText(listaNúmerosTablero.get(2));
+		btnNewButton_2 = new JButton(listaNúmerosTablero.get(2));//Botón 3
 		btnNewButton_2.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_2);
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -216,14 +234,17 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_1.getText().equals("")) {
 					 btnNewButton_1.setText(btnNewButton_2.getText());
+					 
 					 btnNewButton_2.setText("");
 				 }
 				 else if(btnNewButton_3.getText().equals("")) {
 					 btnNewButton_3.setText(btnNewButton_2.getText());
+					 
 					 btnNewButton_2.setText("");
 				 }
 				 else if(btnNewButton_6.getText().equals("")) {
 					 btnNewButton_6.setText(btnNewButton_2.getText());
+					 
 					 btnNewButton_2.setText("");
 				 }
 				 
@@ -235,8 +256,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_3 = new JButton(númerosTablero[3]);//Botón 4
-		//btnNewButton_3.setText(listaNúmerosTablero.get(3));
+		btnNewButton_3 = new JButton(listaNúmerosTablero.get(3));//Botón 4
 		btnNewButton_3.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_3);
 		btnNewButton_3.addActionListener(new ActionListener() {
@@ -246,10 +266,12 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_2.getText().equals("")) {
 					 btnNewButton_2.setText(btnNewButton_3.getText());
+					 
 					 btnNewButton_3.setText("");
 				 }
 				 else if(btnNewButton_7.getText().equals("")) {
 					 btnNewButton_7.setText(btnNewButton_3.getText());
+					 
 					 btnNewButton_3.setText("");
 				 }
 				 
@@ -261,8 +283,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_4 = new JButton(númerosTablero[4]);//Botón 5
-		//btnNewButton_4.setText(listaNúmerosTablero.get(4));
+		btnNewButton_4 = new JButton(listaNúmerosTablero.get(4));//Botón 5
 		btnNewButton_4.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_4);
 		btnNewButton_4.addActionListener(new ActionListener() {
@@ -272,14 +293,17 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton.getText().equals("")) {
 					 btnNewButton.setText(btnNewButton_4.getText());
+					 
 					 btnNewButton_4.setText("");
 				 }
 				 else if(btnNewButton_5.getText().equals("")) {
 					 btnNewButton_5.setText(btnNewButton_4.getText());
+					 
 					 btnNewButton_4.setText("");
 				 }
 				 else if(btnNewButton_8.getText().equals("")) {
 					 btnNewButton_8.setText(btnNewButton_4.getText());
+					 
 					 btnNewButton_4.setText("");
 				 }
 				 
@@ -291,8 +315,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_5 = new JButton(númerosTablero[5]);//Botón 6
-		//btnNewButton_5.setText(listaNúmerosTablero.get(5));
+		btnNewButton_5 = new JButton(listaNúmerosTablero.get(5));//Botón 6
 		btnNewButton_5.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_5);
 		btnNewButton_5.addActionListener(new ActionListener() {
@@ -302,18 +325,22 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_4.getText().equals("")) {
 					 btnNewButton_4.setText(btnNewButton_5.getText());
+					 
 					 btnNewButton_5.setText("");
 				 }
 				 else if(btnNewButton_1.getText().equals("")) {
 					 btnNewButton_1.setText(btnNewButton_5.getText());
+					 
 					 btnNewButton_5.setText("");
 				 }
 				 else if(btnNewButton_6.getText().equals("")) {
 					 btnNewButton_6.setText(btnNewButton_5.getText());
+					 
 					 btnNewButton_5.setText("");
 				 }
 				 else if(btnNewButton_9.getText().equals("")) {
 					 btnNewButton_9.setText(btnNewButton_5.getText());
+					 
 					 btnNewButton_5.setText("");
 				 }
 				 
@@ -325,8 +352,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_6 = new JButton(númerosTablero[6]);//Botón 7
-		//btnNewButton_6.setText(listaNúmerosTablero.get(6));
+		btnNewButton_6 = new JButton(listaNúmerosTablero.get(6));//Botón 7
 		btnNewButton_6.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_6);
 		btnNewButton_6.addActionListener(new ActionListener() {
@@ -336,18 +362,22 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_5.getText().equals("")) {
 					 btnNewButton_5.setText(btnNewButton_6.getText());
+					 
 					 btnNewButton_6.setText("");
 				 }
 				 else if(btnNewButton_2.getText().equals("")) {
 					 btnNewButton_2.setText(btnNewButton_6.getText());
+					 
 					 btnNewButton_6.setText("");
 				 }
 				 else if(btnNewButton_7.getText().equals("")) {
 					 btnNewButton_7.setText(btnNewButton_6.getText());
+					 
 					 btnNewButton_6.setText("");
 				 }
 				 else if(btnNewButton_10.getText().equals("")) {
 					 btnNewButton_10.setText(btnNewButton_6.getText());
+					 
 					 btnNewButton_6.setText("");
 				 }
 				 
@@ -359,8 +389,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_7 = new JButton(númerosTablero[7]);//Botón 8
-		//btnNewButton_7.setText(listaNúmerosTablero.get(7));
+		btnNewButton_7 = new JButton(listaNúmerosTablero.get(7));//Botón 8
 		btnNewButton_7.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_7);
 		btnNewButton_7.addActionListener(new ActionListener() {
@@ -370,18 +399,20 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_3.getText().equals("")) {
 					 btnNewButton_3.setText(btnNewButton_7.getText());
+					 
 					 btnNewButton_7.setText("");
 				 }
 				 else if(btnNewButton_6.getText().equals("")) {
 					 btnNewButton_6.setText(btnNewButton_7.getText());
+					 
 					 btnNewButton_7.setText("");
 				 }
 				 else if(btnNewButton_11.getText().equals("")) {
 					 btnNewButton_11.setText(btnNewButton_7.getText());
+					 
 					 btnNewButton_7.setText("");
 				 }
 				
-				 
 				 tablero.revalidate();
 				 
 				 tablero.repaint();
@@ -390,8 +421,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_8 = new JButton(númerosTablero[8]);//Botón 9
-		//btnNewButton_8.setText(listaNúmerosTablero.get(8));
+		btnNewButton_8 = new JButton(listaNúmerosTablero.get(8));//Botón 9
 		btnNewButton_8.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_8);
 		btnNewButton_8.addActionListener(new ActionListener() {
@@ -401,14 +431,17 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_4.getText().equals("")) {
 					 btnNewButton_4.setText(btnNewButton_8.getText());
+					 
 					 btnNewButton_8.setText("");
 				 }
 				 else if(btnNewButton_9.getText().equals("")) {
 					 btnNewButton_9.setText(btnNewButton_8.getText());
+					 
 					 btnNewButton_8.setText("");
 				 }
 				 else if(btnNewButton_12.getText().equals("")) {
 					 btnNewButton_12.setText(btnNewButton_8.getText());
+					 
 					 btnNewButton_8.setText("");
 				 }
 				
@@ -420,8 +453,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_9 = new JButton(númerosTablero[9]);//Botón 10
-		//btnNewButton_9.setText(listaNúmerosTablero.get(9));
+		btnNewButton_9 = new JButton(listaNúmerosTablero.get(9));//Botón 10
 		btnNewButton_9.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_9);
 		btnNewButton_9.addActionListener(new ActionListener() {
@@ -430,18 +462,22 @@ public class RompecabezasNumérico {
 			public void actionPerformed(ActionEvent e) {
 				 if(btnNewButton_8.getText().equals("")) {
 					 btnNewButton_8.setText(btnNewButton_9.getText());
+					 
 					 btnNewButton_9.setText("");
 				 }
 				 else if(btnNewButton_5.getText().equals("")) {
 					 btnNewButton_5.setText(btnNewButton_9.getText());
+					 
 					 btnNewButton_9.setText("");
 				 }
 				 else if(btnNewButton_10.getText().equals("")) {
 					 btnNewButton_10.setText(btnNewButton_9.getText());
+					 
 					 btnNewButton_9.setText("");
 				 }
 				 else if(btnNewButton_13.getText().equals("")) {
 					 btnNewButton_13.setText(btnNewButton_9.getText());
+					 
 					 btnNewButton_9.setText("");
 				 }
 				 
@@ -453,8 +489,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_10 = new JButton(númerosTablero[10]);//Botón 11
-		//btnNewButton_10.setText(listaNúmerosTablero.get(10));
+		btnNewButton_10 = new JButton(listaNúmerosTablero.get(10));//Botón 11
 		btnNewButton_10.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_10);
 		btnNewButton_10.addActionListener(new ActionListener() {
@@ -464,18 +499,22 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_9.getText().equals("")) {
 					 btnNewButton_9.setText(btnNewButton_10.getText());
+					 
 					 btnNewButton_10.setText("");
 				 }
 				 else if(btnNewButton_6.getText().equals("")) {
 					 btnNewButton_6.setText(btnNewButton_10.getText());
+					 
 					 btnNewButton_10.setText("");
 				 }
 				 else if(btnNewButton_11.getText().equals("")) {
 					 btnNewButton_11.setText(btnNewButton_10.getText());
+					 
 					 btnNewButton_10.setText("");
 				 }
 				 else if(btnNewButton_14.getText().equals("")) {
 					 btnNewButton_14.setText(btnNewButton_10.getText());
+					 
 					 btnNewButton_10.setText("");
 				 }
 				 
@@ -487,8 +526,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_11 = new JButton(númerosTablero[11]);//Botón 12
-		//btnNewButton_11.setText(listaNúmerosTablero.get(11));
+		btnNewButton_11 = new JButton(listaNúmerosTablero.get(11));//Botón 12
 		btnNewButton_11.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_11);
 		btnNewButton_11.addActionListener(new ActionListener() {
@@ -498,14 +536,17 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_10.getText().equals("")) {
 					 btnNewButton_10.setText(btnNewButton_11.getText());
+					 
 					 btnNewButton_11.setText("");
 				 }
 				 else if(btnNewButton_7.getText().equals("")) {
 					 btnNewButton_7.setText(btnNewButton_11.getText());
+					 
 					 btnNewButton_11.setText("");
 				 }
 				 else if(btnNewButton_15.getText().equals("")) {
 					 btnNewButton_15.setText(btnNewButton_11.getText());
+					 
 					 btnNewButton_11.setText("");
 				 }
 				 
@@ -517,8 +558,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_12 = new JButton(númerosTablero[12]);//Botón 13
-		//btnNewButton_12.setText(listaNúmerosTablero.get(12));
+		btnNewButton_12 = new JButton(listaNúmerosTablero.get(12));//Botón 13
 		btnNewButton_12.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_12);
 		btnNewButton_12.addActionListener(new ActionListener() {
@@ -528,10 +568,12 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_8.getText().equals("")) {
 					 btnNewButton_8.setText(btnNewButton_12.getText());
+					 
 					 btnNewButton_12.setText("");
 				 }
 				 else if(btnNewButton_13.getText().equals("")) {
 					 btnNewButton_13.setText(btnNewButton_12.getText());
+					 
 					 btnNewButton_12.setText("");
 				 }
 				 
@@ -543,8 +585,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_13 = new JButton(númerosTablero[13]);//Botón 14
-		//btnNewButton_13.setText(listaNúmerosTablero.get(13));
+		btnNewButton_13 = new JButton(listaNúmerosTablero.get(13));//Botón 14
 		btnNewButton_13.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_13);
 		btnNewButton_13.addActionListener(new ActionListener() {
@@ -554,14 +595,17 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_12.getText().equals("")) {
 					 btnNewButton_12.setText(btnNewButton_13.getText());
+					 
 					 btnNewButton_13.setText("");
 				 }
 				 else if(btnNewButton_9.getText().equals("")) {
 					 btnNewButton_9.setText(btnNewButton_13.getText());
+					 
 					 btnNewButton_13.setText("");
 				 }
 				 else if(btnNewButton_14.getText().equals("")) {
 					 btnNewButton_14.setText(btnNewButton_13.getText());
+					 
 					 btnNewButton_13.setText("");
 				 }
 				 
@@ -573,8 +617,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_14 = new JButton(númerosTablero[14]);//Botón 15
-		//btnNewButton_14.setText(listaNúmerosTablero.get(14));
+		btnNewButton_14 = new JButton(listaNúmerosTablero.get(14));//Botón 15
 		btnNewButton_14.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_14);
 		btnNewButton_14.addActionListener(new ActionListener() {
@@ -584,14 +627,17 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_13.getText().equals("")) {
 					 btnNewButton_13.setText(btnNewButton_14.getText());
+					 
 					 btnNewButton_14.setText("");
 				 }
 				 else if(btnNewButton_10.getText().equals("")) {
 					 btnNewButton_10.setText(btnNewButton_14.getText());
+					 
 					 btnNewButton_14.setText("");
 				 }
 				 else if(btnNewButton_15.getText().equals("")) {
 					 btnNewButton_15.setText(btnNewButton_14.getText());
+					 
 					 btnNewButton_14.setText("");
 				 }
 				 
@@ -603,8 +649,7 @@ public class RompecabezasNumérico {
 			
 			});
 		
-		btnNewButton_15 = new JButton(númerosTablero[15]);//Botón 16
-		//btnNewButton_15.setText(listaNúmerosTablero.get(15));
+		btnNewButton_15 = new JButton(listaNúmerosTablero.get(15));//Botón 16
 		btnNewButton_15.setBackground(new Color(192, 192, 192));
 		tablero.add(btnNewButton_15);
 		btnNewButton_15.addActionListener(new ActionListener() {
@@ -614,10 +659,12 @@ public class RompecabezasNumérico {
 				
 				 if(btnNewButton_14.getText().equals("")) {
 					 btnNewButton_14.setText(btnNewButton_15.getText());
+					 
 					 btnNewButton_15.setText("");
 				 }
 				 else if(btnNewButton_11.getText().equals("")) {
 					 btnNewButton_11.setText(btnNewButton_15.getText());
+					 
 					 btnNewButton_15.setText("");
 				 }
 				
